cf4cdd928637ed5a8437177c8591946c75a271ab
sbarbera4 <35542783+sbarbera4@users.noreply.github.com>
2018-01-30 17:35:04 -0600

    added final paragraph at bottom

--- 99_Bottles.cpp
+++ 99_Bottles.cpp
@@ -19,10 +19,20 @@ int main()
 	cout << "take one down, pass it around," << endl;
 	cout << --bottles << " bottle of beer on the wall." << endl;
 	cout << " " << endl;
+	
 	cout << bottles << " bottle of beer on the wall," << endl;
 	cout << bottles << " bottle of beer." << endl;
 	cout << "take one down, pass it around," << endl;
-	cout << --bottles << " bottles of beer on the wall." << endl;
+	cout << "no more bottles of beer on the wall." << endl;
+	cout << " " << endl;
+	
+	cout << "no more bottles of beer on the wall," << endl;
+	cout << "no more bottles of beer." << endl;
+	cout << "go to the store and buy some more," << endl;
+	cout << "99 bottles of beer on the wall." << endl;
+	cout << " " << endl;
+	
+	
 	
 	
 	return 0;
